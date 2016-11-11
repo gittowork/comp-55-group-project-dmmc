2415095d0b25302fb01bfb720f7ab28fd2d3df18
Brendan <b_ahdoot@u.pacific.edu>
2016-11-10 16:12:43 -0800

    Added Velocity and Acceleration

--- src/DMMC/Game.java
+++ src/DMMC/Game.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import DMMC.Screen.Screen;
-import acm.graphics.GPoint;
+import DMMC.Physics.*;
+import acm.graphics.GImage;
 import acm.program.GraphicsProgram;
 
 public class Game extends GraphicsProgram{
@@ -14,16 +15,18 @@ public class Game extends GraphicsProgram{
 	
 	private static final long serialVersionUID = 1L;
 	public static final double GRAVITY = 9.8;
+	public static final int windowHeight = 600, windowWidth = 400;
 	
 	private Screen currentScreen; 
 	private ArrayList<Profile> profiles;
 	private int currentUser; // position of user in list^
 	private HashMap<String, Image[]> animations;
 	private GameState gameState;
+	Entity e;
 	
 	public void init()
 	{
-		
+		this.resize(windowWidth, windowHeight);
 		profiles = new ArrayList<Profile>();
 		currentUser = -1;
 		animations = new HashMap<String, Image[]>();
@@ -31,12 +34,21 @@ public class Game extends GraphicsProgram{
 		
 		System.out.println(gameState.toString());
 		
+		e = new Entity(new GImage("TESTIMAGE.jpg"));
+		add(e.getScreenObj());
 	}
 	
 	public void run()
 	{
 		System.out.println("RUN");
+		while(true){
+			e.update();
+		}
 		
+	}
+	
+	public static double deltaTime(){
+		return 1/60;
 	}
 
 }
