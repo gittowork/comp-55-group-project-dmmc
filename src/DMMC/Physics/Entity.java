aa6ce0034a11a1dc332fb04c2667dbf7c3688cec
pranavt97 <p_thirunavukkarasu@u.pacific.edu>
2016-11-16 14:54:57 -0800

    clean up: added comments, split up work into helpers, converted shorts to chars.

--- src/DMMC/Physics/Entity.java
+++ src/DMMC/Physics/Entity.java
@@ -17,32 +17,33 @@ public class Entity extends PhysicsObject{
 
 	protected GPoint velocity;
 	protected GPoint acceleration;
-	private short id;
-	protected short curHealth;
-	protected short maxHealth;
+	private char id;
+	protected char curHealth;
+	protected char maxHealth;
 	protected boolean weightless;
 	protected boolean collidable;
 	protected CollisionPoint[] colPoints;
-	protected CollisionPoint[] cornerPoints;
-	protected short[] colIndex;
+	protected char[] colIndex;
+	protected char[] indexOrder;
 	private GPoint collisionTilePoint;
 
 	public Entity(GImage i, Image[] initAnimation) {
 		super(i, initAnimation);
 		initPoints();
-
+		setColPoint();
 	}
 
 	public Entity(GImage i) {
 		super(i);
 		initPoints();
+		setColPoint();
 	}
 
 	public double getVelX(){return velocity.getX();}
 	public double getVelY(){return velocity.getY();}
 	public double getAccX(){return acceleration.getX();}
 	public double getAccY(){return acceleration.getY();}
-	public short getId(){return id;}
+	public char getId(){return id;}
 	public boolean isWeightless() {return weightless;}
 	public boolean isCollideable() {return collidable;}
 
@@ -55,109 +56,33 @@ public class Entity extends PhysicsObject{
 
 	public void update(){
 
-
-		setColPoint();
-
 		//set desired velocity before collision
 		setAccY(Game.GRAVITY /Game.FPS);
 		setVelX(getVelX() + getAccX());
 		setVelY(getVelY() + getAccY());
 		setVelX(0.5);
-
-		short indexOrder[] = new short[4];
-		for(int i = 0; i < indexOrder.length; i ++)
-		{
-			indexOrder[i] = -1;
-		}
-
-		shortColIndex(indexOrder);
-		System.out.println("FRAME:");
-		for(short i = 0; i < indexOrder.length; i ++)
-		{
-			if(colIndex[indexOrder[i]] == 0)
-				break;
-			if(indexOrder[i] == 0)
-			{
-				if(colPoints[0].getColliding())
-					collisionTilePoint =  Game.tilePosToScreen(colPoints[0].getTileX(), colPoints[0].getTileY());
-				else if(colPoints[1].getColliding())
-					collisionTilePoint =  Game.tilePosToScreen(colPoints[1].getTileX(), colPoints[1].getTileY()); 
-				else 
-					break;
-				
-				screenObj.setLocation(getScreenPosX(), collisionTilePoint.getY() + Game.tileHeight);
-				System.out.println("TOP");
-				setVelY(0);
-			}
-			else if(indexOrder[i] == 1)
-			{
-				if(colPoints[2].getColliding())
-					collisionTilePoint =  Game.tilePosToScreen(colPoints[2].getTileX(), colPoints[2].getTileY());
-				else if(colPoints[3].getColliding())
-					collisionTilePoint =  Game.tilePosToScreen(colPoints[3].getTileX(), colPoints[3].getTileY()); 
-				else 
-					break;
-
-				screenObj.setLocation(collisionTilePoint.getX() - screenObj.getWidth(), getScreenPosY());
-				System.out.println("RIGHT");
-				setVelX(0);
-			}
-			else if(indexOrder[i] == 2)
-			{
-				if(colPoints[4].getColliding())
-					collisionTilePoint =  Game.tilePosToScreen(colPoints[4].getTileX(), colPoints[4].getTileY());
-				else if(colPoints[5].getColliding())
-					collisionTilePoint =  Game.tilePosToScreen(colPoints[5].getTileX(), colPoints[5].getTileY()); 
-				else 
-					break;
-
-				screenObj.setLocation(screenObj.getX(), collisionTilePoint.getY() - screenObj.getHeight());
-				System.out.println("BOT");
-				setVelY(0);
-			}
-			else if(indexOrder[i] == 3)
-			{
-				if(colPoints[6].getColliding())
-					collisionTilePoint =  Game.tilePosToScreen(colPoints[6].getTileX(), colPoints[6].getTileY());
-				else if(colPoints[7].getColliding())
-					collisionTilePoint =  Game.tilePosToScreen(colPoints[7].getTileX(), colPoints[7].getTileY()); 
-				else 
-					break;
-
-				screenObj.setLocation(collisionTilePoint.getX() + Game.tileWidth , getScreenPosY());
-				System.out.println("LEFT");
-				setVelX(0);
-			}
-			System.out.println("SET");
-			setColPoint();
-			shortColIndex(indexOrder);
-		}
+		repositionAfterCol();
+		
 		screenObj.move(getVelX(), getVelY());
-
-
-		System.out.println("OUT\n");
 
 	}
 	
-	private void shortColIndex(short[] indexOrder)
+	//gets an array of the indexes of colindex in order of its contents
+	private void sortColIndex(char[] indexOrder)
 	{
 		boolean sorting = true;
 		short lookNum = 2;
 		short j = 0;
+		
 		while(sorting)
 		{
-			for(short i = 0; i < indexOrder.length; i ++)
-			{
+			for(char i = 0; i < indexOrder.length; i ++)
 				if(colIndex[i] == lookNum)
-				{
 					indexOrder[j++] = i;
-				}
-			}
 			lookNum--;
 			if(j == 4)
 				sorting = false;
 		}
-		
 	}
 
 	private void initPoints(){
@@ -169,14 +94,12 @@ public class Entity extends PhysicsObject{
 		colPoints = new CollisionPoint[8];
 		for(int i = 0; i < colPoints.length; i ++)
 			colPoints[i] = new CollisionPoint(0,0);
-
-		//Corner Points
-		cornerPoints = new CollisionPoint[4];
-		for(int i = 0; i < cornerPoints.length; i ++)
-			cornerPoints[i] = new CollisionPoint(0,0);
-
+		
 		//index for every side of the object, counts how many collisions per side
-		colIndex = new short[4];
+		colIndex = new char[4];
+		
+		// stores indexes of colIndex^
+		indexOrder = new char[4];
 
 	}
 
@@ -198,26 +121,19 @@ public class Entity extends PhysicsObject{
 		colPoints[6].setLocation(getScreenPosX() - colPointPadding, getScreenPosY() + screenObj.getHeight() - colPointPadding);
 		colPoints[7].setLocation(getScreenPosX() - colPointPadding, getScreenPosY() + colPointPadding);
 
-		//Top Right
-		cornerPoints[0].setLocation(getScreenPosX(), getScreenPosY());
-
-		//Top Left
-		cornerPoints[1].setLocation(getScreenPosX() + screenObj.getWidth(), getScreenPosY());
-
-		//Bot Left
-		cornerPoints[2].setLocation(getScreenPosX() + screenObj.getWidth(), getScreenPosY() + screenObj.getHeight());
-
-		//Bot Right
-		cornerPoints[3].setLocation(getScreenPosX(),  getScreenPosY() + screenObj.getHeight());
-
 		checkForCollision();
 	}
 	private void checkForCollision()
 	{
-		//reset col index
+		//reset indexOrder
+		for(char i = 0; i < indexOrder.length; i ++)
+			indexOrder[i] = (char) -1;
+		
+		//reset colindex
 		for(short i = 0; i < colIndex.length; i ++)
 			colIndex[i] = 0;
 
+		//get collisions
 		for(short i = 0; i < colPoints.length; i ++)
 		{
 			colPoints[i].setColliding(Game.isPointOnSolid(colPoints[i]));
@@ -226,13 +142,87 @@ public class Entity extends PhysicsObject{
 			if(colPoints[i].getColliding())
 				colIndex[Math.floorDiv(i, 2)] ++; 
 		}
-		for(short i = 0; i < cornerPoints.length; i ++)
-			cornerPoints[i].setColliding(Game.isPointOnSolid(cornerPoints[i]));
 		
-		System.out.println("Cur Col: ");
-		for(int i = 0; i < colIndex.length; i ++)
-			System.out.print(colIndex[i]);
-		System.out.println("");
+		//sort indexes
+		sortColIndex(indexOrder);
+	}
+	
+	private void repositionAfterCol()
+	{
+		for(short i = 0; i < indexOrder.length; i ++)
+		{
+			//reset collision points to see if any more repositioning is needed
+			//TODO: Being called every 4 times a frame every time, make set location function, call only when location is set or moved
+			setColPoint();
+			
+			//if not collision on side, leave
+			if(colIndex[indexOrder[i]] == 0)
+				break;
+			
+			//Top Collision
+			if(indexOrder[i] == 0)
+			{
+				//Get tile that is colliding
+				if(colPoints[0].getColliding())
+					collisionTilePoint =  Game.tilePosToScreen(colPoints[0].getTileX(), colPoints[0].getTileY());
+				else if(colPoints[1].getColliding())
+					collisionTilePoint =  Game.tilePosToScreen(colPoints[1].getTileX(), colPoints[1].getTileY()); 
+				else 
+					break;
+				
+				//reset position
+				screenObj.setLocation(getScreenPosX(), collisionTilePoint.getY() + Game.tileHeight);
+				setVelY(0);
+			}
+			
+			//Right Collision
+			else if(indexOrder[i] == 1)
+			{
+				//Get tile that is colliding
+				if(colPoints[2].getColliding())
+					collisionTilePoint =  Game.tilePosToScreen(colPoints[2].getTileX(), colPoints[2].getTileY());
+				else if(colPoints[3].getColliding())
+					collisionTilePoint =  Game.tilePosToScreen(colPoints[3].getTileX(), colPoints[3].getTileY()); 
+				else 
+					break;
+
+				//reset position
+				screenObj.setLocation(collisionTilePoint.getX() - screenObj.getWidth(), getScreenPosY());
+				setVelX(0);
+			}
+			
+			//Bot Collision
+			else if(indexOrder[i] == 2)
+			{
+				//Get tile that is colliding
+				if(colPoints[4].getColliding())
+					collisionTilePoint =  Game.tilePosToScreen(colPoints[4].getTileX(), colPoints[4].getTileY());
+				else if(colPoints[5].getColliding())
+					collisionTilePoint =  Game.tilePosToScreen(colPoints[5].getTileX(), colPoints[5].getTileY()); 
+				else 
+					break;
+
+				//reset position
+				screenObj.setLocation(screenObj.getX(), collisionTilePoint.getY() - screenObj.getHeight());
+				setVelY(0);
+			}
+			
+			//Left Collision
+			else if(indexOrder[i] == 3)
+			{
+				//Get tile that is colliding
+				if(colPoints[6].getColliding())
+					collisionTilePoint =  Game.tilePosToScreen(colPoints[6].getTileX(), colPoints[6].getTileY());
+				else if(colPoints[7].getColliding())
+					collisionTilePoint =  Game.tilePosToScreen(colPoints[7].getTileX(), colPoints[7].getTileY()); 
+				else 
+					break;
+
+				//reset position
+				screenObj.setLocation(collisionTilePoint.getX() + Game.tileWidth , getScreenPosY());
+				setVelX(0);
+			}
+		}
 	}
 	//	public abstract void spawnAction();
 	//	public abstract void deathAction();
