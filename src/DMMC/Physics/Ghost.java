f52e141d17ed0bb0c7bb0dabd797591b92cf246c
pranavt97 <p_thirunavukkarasu@u.pacific.edu>
2016-12-06 18:42:13 -0800

    ghost charges

--- src/DMMC/Physics/Ghost.java
+++ src/DMMC/Physics/Ghost.java
@@ -14,14 +14,16 @@ public class Ghost extends Entity {
 	GImage Sprite;
 	static final String GHOST_FILE_PATH = "";
 	GPoint playerLoc;
-	private final double SPEED = 1;
+	private double SPEED = 1;
 	private static final int sizeX = 50;
 	private static final int sizeY = 50;
+	private GPoint chargeTo;
+	private boolean charge;
 	
 	public Ghost(int id){
 		super ("ghost-idle",id, true, false);	
 		scaleScreenObj();
-		animationSpeed = 60;
+		animationSpeed = 45;
 	}
 
 	@Override
@@ -38,16 +40,41 @@ public class Ghost extends Entity {
 
 	@Override
 	public void behaviorAction() {
-		double xDifference,yDifference, hypotnuse, ratio;
+double xDifference,yDifference, distance, ratio;
 		
 		playerLoc = Game.player.getScreenPos();
 		
 		xDifference = playerLoc.getX() - getScreenPosX();
 		yDifference = playerLoc.getY() - getScreenPosY();
 		
-		hypotnuse =Math.pow(Math.pow(xDifference,2) + Math.pow(yDifference,2), (0.5));
-		if (hypotnuse != 0)
-			ratio = SPEED / hypotnuse;
+		distance = Math.pow(Math.pow(xDifference,2) + Math.pow(yDifference,2), (0.5));
+				
+		if(!charge)
+		{
+			charge = (distance < 130);
+			if(charge)
+				chargeTo = playerLoc;
+			else
+				chargeTo = new GPoint(-1, -1);
+			
+			animationSpeed = 45;
+			SPEED = 1;
+
+		}
+		else
+		{
+			xDifference = chargeTo.getX() - getScreenPosX();
+			yDifference = chargeTo.getY() - getScreenPosY();
+			distance = Math.pow(Math.pow(xDifference,2) + Math.pow(yDifference,2), (0.5));
+			
+			animationSpeed = 10;
+			SPEED = 3;
+			
+			charge = (distance > 2);
+		}
+		
+		if (distance != 0)
+			ratio = SPEED / distance;
 		else
 			ratio = 0;
 		
