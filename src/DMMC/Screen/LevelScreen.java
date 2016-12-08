88c913d6c8663b8ffe2ae72541f79ddf343f8255
Brendan <b_ahdoot@u.pacific.edu>
2016-12-07 21:23:22 -0800

    Removed debugging

--- src/DMMC/Screen/LevelScreen.java
+++ src/DMMC/Screen/LevelScreen.java
@@ -68,7 +68,7 @@ public class LevelScreen extends Screen{
 		initTiles(oneDChartoTwoDChar(levelData[2].toCharArray(), 
 				levelSizeX, levelSizeY));
 
-		loadWave(3);
+		loadWave(0);
 		kernelsToBeSpawned = new ArrayList<GPoint>();
 		lastWaveEs = new ArrayList<Entity>();
 	}
